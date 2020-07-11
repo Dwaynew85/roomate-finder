a5c7cced3732242a2acc2f02eb11ef7a77cdef68
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-11 04:52:39 +0000

    added scraper & room methods and README file

--- lib/room_scraper.rb
+++ lib/room_scraper.rb
@@ -2,12 +2,29 @@ class RoomScraper
   index_url = "https://atlanta.craigslist.org/search/roo"
 
   def initialize(index_url)
+    @index_url = index_url
     @doc = Nokogiri::HTML(open(index_url))
-    binding.pry
   end
 
+  def call
+    rows.each do |row_doc|
+      Room.create_from_hash(scrape_row(row_doc)) #=> Should put the room in my database
+    end
+  end
 
+  private
   def rows
     @rows ||= @doc.search("p.result-info")
   end
+
+  def scrape_row(row)
+    # scrape an individual row and return a hash
+    {
+    :date_created => row.search("time").first.attributes["datetime"].text,
+    :title => row.search("a.hdrlnk").text,
+    :url => row.search("a.hdrlnk").attribute("href").text,
+    :price => row.search("span.result-price").text,
+    }
+  end
+
 end
