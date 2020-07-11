9afc470c488fea609ff30e53b7cb95e6f628d720
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-11 05:30:28 +0000

    added room methods

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,8 @@ Bundler.require
 
 require 'open-uri'
 
+DB = {
+  :connection => SQLite3::Database.new("db/rooms-development.sqlite")
+}
+
 require_all 'lib'
--- lib/room.rb
+++ lib/room.rb
@@ -15,7 +15,28 @@ class Room
   end
 
   def save
+    insert
+  end
 
+  def insert
+    sql = <<-SQL
+    INSERT INTO rooms (title, date_created, price, url) VALUES (?, ?, ?, ?)
+    SQL
+    DB[:connection].execute(sql, self.title, self.date_created, self.price, self.url)
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS rooms (
+      id INTEGER PRIMARY KEY,
+      title TEXT,
+      date_created DATETIME,
+      price TEXT,
+      url TEXT
+    )
+    SQL
+
+    DB[:connection].execute(sql)
   end
 
 end
